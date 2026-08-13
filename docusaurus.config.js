ac3303fc34a3a8e02128531755942435ce7402cb
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2026-08-13 18:06:46 +0200

    chore: rename social card to bust cache

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/social-card.png",
+      image: "img/social-card-v2.png",
       navbar: {
         hideOnScroll: false,
         items: [
